f471f7d4b7351dfce63edae866a08361908b5bf5
Syed Hussaini <shussaini@financeit.io>
2020-06-09 18:38:11 -0400

    updated

--- siteConfig.js
+++ siteConfig.js
@@ -1,9 +1,10 @@
 module.exports = {
-  name: 'London Night and Day',
-  shortName: '',
-  description: 'A bold, minimal theme for Gatsby, focused on clean typography & beautiful imagery',
-  url: 'https://www.jooplaan.com', // Domain of your site without prefix!
-  prefix: '/',
-  author: 'jooplaan',
-  twitter: ''
-}
+  name: "The Alisha Jilani Foundation",
+  shortName: "",
+  description:
+    "The Alisha Jilani Foundation. Founded in memory of Alisha Jilani.",
+  url: "https://www.jooplaan.com", // Domain of your site without prefix!
+  prefix: "/",
+  author: "jooplaan",
+  twitter: ""
+};
